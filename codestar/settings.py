de8f0b14fd046d98af282af7755f35462c81236f
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-06 11:01:14 +0100

    change debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_7p13fklsi4kw=f&r2x+=3u)ob5f31yi!^7b(v7fioox&=*q1)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'coates-new-blog-acd04e10d42a.herokuapp.com',]
 
