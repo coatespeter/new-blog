d8d0d5dcc1280c34d6251c857436cc0af079e7ac
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-09 14:50:05 +0100

    ready for deployment

---
+++ about/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .models import About
+from .forms import CollaborateForm
+
+def about_me(request):
+    """
+    Renders the About page
+    """
+    # Define collaborate_form and about outside of the if block
+    collaborate_form = CollaborateForm()
+    about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+            # Re-fetch the about object after saving the form
+            about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form,
+        },
+    )
