2b0103a5f30cff4ab52358cf394cbb37f5427ebc
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-09 12:51:04 +0100

    collaboration form added to about page

--- about/forms.py
+++ about/forms.py
@@ -1,7 +1,20 @@
-from blog.models import Comment
+from .models import CollaborateRequest
 from django import forms
+from blog.models import Comment
+
 
 class CommentForm(forms.ModelForm):
+    """
+    Form class for users to comment on a post
+    """
     class Meta:
+        """
+        Specify the django model and order of the fields
+        """
         model = Comment
         fields = ('body',)
+
+class CollaborateForm(forms.ModelForm):
+    class Meta:
+        model = CollaborateRequest
+        fields = ('name', 'email', 'message')
